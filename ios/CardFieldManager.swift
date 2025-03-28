a8d87c2b73038c51ad2654c0bbe419b1b047e871
Anton <antonvotinov@gmail.com>
2025-03-28 11:52:02 +0600

    fix: change cardField

--- ios/CardFieldManager.swift
+++ ios/CardFieldManager.swift
@@ -5,31 +5,31 @@ class CardFieldManager: RCTViewManager {
     override func view() -> UIView! {
         let cardField = CardFieldView()
         let stripeSdk = bridge.module(forName: "StripeSdk") as? StripeSdk
-        stripeSdk?.cardFieldView = cardField;
+        stripeSdk?.cardFieldView = cardField
         return cardField
     }
-    
+
     @objc func focus(_ reactTag: NSNumber) {
-        self.bridge!.uiManager.addUIBlock { (_: RCTUIManager?, viewRegistry: [NSNumber: UIView]?) in
-            let view: CardFieldView = (viewRegistry![reactTag] as? CardFieldView)!
+        self.bridge?.uiManager.addUIBlock { (_, viewRegistry) in
+            guard let view = viewRegistry?[reactTag] as? CardFieldView else { return }
             view.focus()
         }
     }
-    
+
     @objc func blur(_ reactTag: NSNumber) {
-        self.bridge!.uiManager.addUIBlock { (_: RCTUIManager?, viewRegistry: [NSNumber: UIView]?) in
-            let view: CardFieldView = (viewRegistry![reactTag] as? CardFieldView)!
+        self.bridge?.uiManager.addUIBlock { (_, viewRegistry) in
+            guard let view = viewRegistry?[reactTag] as? CardFieldView else { return }
             view.blur()
         }
     }
-    
+
     @objc func clear(_ reactTag: NSNumber) {
-        self.bridge!.uiManager.addUIBlock { (_: RCTUIManager?, viewRegistry: [NSNumber: UIView]?) in
-            let view: CardFieldView = (viewRegistry![reactTag] as? CardFieldView)!
+        self.bridge?.uiManager.addUIBlock { (_, viewRegistry) in
+            guard let view = viewRegistry?[reactTag] as? CardFieldView else { return }
             view.clear()
         }
     }
-    
+
     override class func requiresMainQueueSetup() -> Bool {
         return false
     }
